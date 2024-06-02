20accbfbdfe09bcdf88f2baf94ae78214c2659f1
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2024-06-02 18:46:12 +0600

    stash

--- lib/Pages/home_page/home_page.dart
+++ lib/Pages/home_page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dash_and_tag_web_site/Pages/footer/footer.dart';
 import 'package:dash_and_tag_web_site/controller/main_controller.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'about_us.dart';
@@ -19,10 +20,48 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Dash&Tag'),
         actions: [
-          // ...List.generate(controller.appbarActions.length, (index) {
-          //   final action = controller.appbarActions[index];
-          //   return DropdownButtonHideUnderline(child: DropdownButton2());
-          // }),
+          ...List.generate(controller.appbarActions.length, (index) {
+            final action = controller.appbarActions[index];
+            return MouseRegion(
+              cursor: SystemMouseCursors.click,
+              onHover: (event) {
+                //show subcategories
+                if (action.categories != null) {
+                  PopupMenuButton(
+                    itemBuilder: (context) {
+                      return action.categories!
+                          .map((e) => PopupMenuItem(
+                                child: Text(e.title),
+                              ))
+                          .toList();
+                    },
+                  );
+                }
+              },
+              child: DropdownButtonHideUnderline(
+                  child: action.categories != null
+                      ? DropdownButton2(
+                          items: action.categories!
+                              .map((e) => DropdownMenuItem(
+                                    value: e.title,
+                                    child: Row(
+                                      children: [
+                                        Text(e.title),
+                                        if (e.subCategories != null)
+                                          Icon(Icons.arrow_forward_ios)
+                                      ],
+                                    ),
+                                  ))
+                              .toList(),
+                          onChanged: (value) {
+                            // controller.onActionTap(action.title, value.toString());
+                            printInfo(info: value.toString());
+                          },
+                          hint: Text(action.title),
+                        )
+                      : Container()),
+            );
+          }),
           const FooterBottomSocialButtons(),
         ],
       ),
