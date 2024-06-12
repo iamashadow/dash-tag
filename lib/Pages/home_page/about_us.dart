f7e01ffb08af062bc8b64b7f406e3cfd5bda1db1
iamashadow <rockzer420@gmail.com>
2024-06-12 17:22:53 +0600

    Initial Version 1 -> Handed over to sifat for responsiveness

--- lib/Pages/home_page/about_us.dart
+++ lib/Pages/home_page/about_us.dart
@@ -23,98 +23,90 @@ class AboutUsPage extends StatelessWidget {
       width: double.maxFinite,
       padding: const EdgeInsets.only(left: 30, right: 50),
       color: const Color(0xFFF4F0EC).withOpacity(0.5),
-      child: ResponsiveRowColumn(
-        rowMainAxisAlignment: MainAxisAlignment.center,
-        rowCrossAxisAlignment: CrossAxisAlignment.center,
-        layout: ResponsiveBreakpoints.of(context).largerThan(DESKTOP) ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // layout: ResponsiveBreakpoints.of(context).largerThan(TABLET) ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.COLUMN,
         children: [
 
-           const ResponsiveRowColumnItem(child: SizedBox(height: 50,),),
-          ResponsiveRowColumnItem(
-            child: CustomText( 
-              title: HomePageText.aboutUsResources,
-              letterSpacing: 7,
-              fontColor: Colors.blue,
-              fontSize: 20,
-              fontFamily: "Rajdhani",
-            ),
+           const SizedBox(height: 50,),
+          CustomText(
+            title: HomePageText.aboutUsResources,
+            letterSpacing: 7,
+            fontColor: Colors.blue,
+            fontSize: 20,
+            fontFamily: "Rajdhani",
           ),
-          ResponsiveRowColumnItem(
-            child: CustomText(
-              title: HomePageText.aboutUs,
-              letterSpacing: 2,
-              fontColor: Colors.deepOrangeAccent,
-              fontSize: 50,
-              fontFamily: "Caveat",
-            ),
+          CustomText(
+            title: HomePageText.aboutUs,
+            letterSpacing: 2,
+            fontColor: Colors.deepOrangeAccent,
+            fontSize: 50,
+            fontFamily: "Caveat",
           ),
 
 
-          ResponsiveRowColumnItem(
-            child: Expanded(
-              child: ResponsiveRowColumn(
-                rowMainAxisAlignment: MainAxisAlignment.center,
-                rowCrossAxisAlignment: CrossAxisAlignment.center,
-                layout: ResponsiveBreakpoints.of(context).largerThan(DESKTOP) ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
-                children: [
-                  ResponsiveRowColumnItem(
-                    child: Expanded(
-                      flex: 1,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          CustomText(
-                            title: HomePageText.aboutUsArtAndFashion,
-                            fontSize: 50,
-                            textAlign: TextAlign.center,
-                            fontWeight: FontWeight.bold,
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // layout: ResponsiveBreakpoints.of(context).largerThan(TABLET) ? ResponsiveRowColumnType.ROW : ResponsiveRowColumnType.COLUMN,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomText(
+                        title: HomePageText.aboutUsArtAndFashion,
+                        fontSize: 50,
+                        textAlign: TextAlign.center,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Rajdhani",
+                      ),
+                      Card(
+                        elevation: 5,
+                        child: Container(
+                          width: 500,
+                          height: 400,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage("assets/images/means_jeans/Picture1.png",)
+                            )
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(width: 20,),
+
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: CustomText(
+                            title: HomePageText.aboutUsDescription,
+                            fontColor: Colors.black,
+                            textAlign: TextAlign.start,
+                            fontSize: 20,
                             fontFamily: "Rajdhani",
                           ),
-                          Card(
-                            elevation: 5,
-                            child: Container(
-                              width: 500,
-                              height: 400,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                image: const DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: AssetImage("assets/images/means_jeans/Picture1.png",)
-                                )
-                              ),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-            
-                  ResponsiveRowColumnItem(
-                    child: Expanded(
-                      flex: 2,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: CustomText(
-                                title: HomePageText.aboutUsDescription,
-                                fontColor: Colors.black,
-                                textAlign: TextAlign.start,
-                                fontSize: 20,
-                                fontFamily: "Rajdhani",
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ],
