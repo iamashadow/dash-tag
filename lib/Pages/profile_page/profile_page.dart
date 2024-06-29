32673c7031b80c8f66bf29b65c6fc165911eef46
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2024-06-29 12:25:59 +0600

    profile pdf done

--- lib/Pages/profile_page/profile_page.dart
+++ lib/Pages/profile_page/profile_page.dart
@@ -1,45 +1,41 @@
-import 'package:dash_and_tag_web_site/Utils/Profile_Pdf/profile_pdf.dart';
-import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:dash_and_tag_web_site/Universal_Widgets/custom_drawer.dart';
+import 'package:dash_and_tag_web_site/controller/main_controller.dart';
+import 'package:dash_and_tag_web_site/utils/all_colors/all_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
-import '../../Utils/All_Colors/all_colors.dart';
-import '../../controller/main_controller.dart';
-import '../home_page/component/desktop/widgets/footer_bottom_social_buttons.dart';
-import '../home_page/view/desktop/home_page_desktop.dart';
+import 'package:universal_html/html.dart' as html;
+import '../../Universal_Widgets/custom_appbar.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
-
+  ProfilePage({super.key});
+  final MainController controller = Get.put(MainController());
   @override
   Widget build(BuildContext context) {
-    final MainController controller = Get.put(MainController());
-
     return Scaffold(
-      backgroundColor: ColorManager.contactUsPageWebBackgroundColor,
-
-      // body: ListView(
-      //   children: [
-      //     ProductsPageHeaderImage(
-      //       title: "Company Profile",
-      //     ),
-      //     const SizedBox(height: 50),
-      //
-      //     Padding(
-      //       padding:  const EdgeInsets.all(100.0),
-      //       child: SfPdfViewer.asset(ShowProfilePdf.profilePdfLink),
-      //     ),
-      //
-      //     const SizedBox(height: 250),
-      //
-      //     const Footer(),
-      //   ],
-      // ),
-
-      body: SfPdfViewer.asset(
-        ShowProfilePdf.profilePdfLink,
-        canShowPaginationDialog: false,
-      ),
-    );
+        backgroundColor: ColorManager.contactUsPageWebBackgroundColor,
+        drawer: CustomDrawer(controller: controller),
+        body: Column(
+          children: [
+            //header appbar
+            CustomAppbar(controller: controller),
+            Expanded(
+              child: SfPdfViewer.asset(
+                "assets/images/profile_pdf/mypdf.pdf",
+                canShowPaginationDialog: false,
+              ),
+            ),
+          ],
+        ),
+        //download button
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //download pdf
+            html.AnchorElement(href: 'assets/images/profile_pdf/mypdf.pdf')
+      ..setAttribute("download", "mypdf.pdf")
+      ..click();
+          },
+          child: Icon(Icons.download),
+        ));
   }
 }
