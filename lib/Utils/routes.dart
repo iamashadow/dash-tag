ea20d3bd7091ad9aa224f8ab612a9e2c65b2b49a
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2024-07-01 16:51:40 +0600

    conflict

--- lib/Utils/routes.dart
+++ lib/Utils/routes.dart
@@ -11,59 +11,56 @@ import '../Pages/our_clients_page/our_clients_root.dart';
 import '../Pages/services_sector/services_page_root.dart';
 
 class AppRoutes {
-  static String home = '/home';
-  static String aboutResources = '/about_resources';
-  static String missionVision = '/mission_vision';
-  static String profile = '/profile';
-  static String mens = '/products/mens';
-  static String mensjeans = '/products/mens/jeans';
-  static String menstshirts = '/products/mens/tshirts';
-  static String menspoloshirts = '/products/mens/poloshirts';
-  static String mensshirtspants = '/products/mens/shirtpants';
-  static String menshoodies = '/products/mens/hoodies';
-  static String mensshortscargo = '/products/mens/shortscargo';
-  static String mensjackets = '/products/mens/jackets';
-  static String menssweaters = '/products/mens/sweaters';
+  static const String home = '/home';
+  static const String aboutResources = '/about_resources';
+  static const String missionVision = '/mission_vision';
+  static const String profile = '/profile';
+  static const String mens = '/products/mens';
+  static const String mensjeans = '/products/mens/jeans';
+  static const String menstshirts = '/products/mens/tshirts';
+  static const String menspoloshirts = '/products/mens/poloshirts';
+  static const String mensshirtspants = '/products/mens/shirtpants';
+  static const String menshoodies = '/products/mens/hoodies';
+  static const String mensshortscargo = '/products/mens/shortscargo';
+  static const String mensjackets = '/products/mens/jackets';
+  static const String menssweaters = '/products/mens/sweaters';
   //same for womens
-  static String womens = '/products/womens';
-  static String womensjeans = '/products/womens/jeans';
-  static String womenstshirts = '/products/womens/tshirts';
-  static String womenspoloshirts = '/products/womens/poloshirts';
-  static String womensshirtspants = '/products/womens/shirtpants';
-  static String womenshoodies = '/products/womens/hoodies';
-  static String womensshortscargo = '/products/womens/shortscargo';
-  static String womensjackets = '/products/womens/jackets';
-  static String womenssweaters = '/products/womens/sweaters';
+  static const String womens = '/products/womens';
+  static const String womensjeans = '/products/womens/jeans';
+  static const String womenstshirts = '/products/womens/tshirts';
+  static const String womenspoloshirts = '/products/womens/poloshirts';
+  static const String womensshirtspants = '/products/womens/shirtpants';
+  static const String womenshoodies = '/products/womens/hoodies';
+  static const String womensshortscargo = '/products/womens/shortscargo';
+  static const String womensjackets = '/products/womens/jackets';
+  static const String womenssweaters = '/products/womens/sweaters';
   //same for boys
-  static String boys = '/products/boys';
-  static String boysjeans = '/products/boys/jeans';
-  static String boystshirts = '/products/boys/tshirts';
-  static String boyspoloshirts = '/products/boys/poloshirts';
-  static String boysshirtspants = '/products/boys/shirtpants';
-  static String boyshoodies = '/products/boys/hoodies';
-  static String boyshortscargo = '/products/boys/shortscargo';
-  static String boysjackets = '/products/boys/jackets';
-  static String boyssweaters = '/products/boys/sweaters';
+  static const String boys = '/products/boys';
+  static const String boysjeans = '/products/boys/jeans';
+  static const String boystshirts = '/products/boys/tshirts';
+  static const String boyspoloshirts = '/products/boys/poloshirts';
+  static const String boysshirtspants = '/products/boys/shirtpants';
+  static const String boyshoodies = '/products/boys/hoodies';
+  static const String boyshortscargo = '/products/boys/shortscargo';
+  static const String boysjackets = '/products/boys/jackets';
+  static const String boyssweaters = '/products/boys/sweaters';
   //same for girls
-  static String girls = '/products/girls';
-  static String girlsjeans = '/products/girls/jeans';
-  static String girlstshirts = '/products/girls/tshirts';
-  static String girlspoloshirts = '/products/girls/poloshirts';
-  static String girlsshirtspants = '/products/girls/shirtpants';
-  static String girlshoodies = '/products/girls/hoodies';
-  static String girlshortscargo = '/products/girls/shortscargo';
-  static String girlsjackets = '/products/girls/jackets';
-  static String girlssweaters = '/products/girls/sweaters';
+  static const String girls = '/products/girls';
+  static const String girlsjeans = '/products/girls/jeans';
+  static const String girlstshirts = '/products/girls/tshirts';
+  static const String girlspoloshirts = '/products/girls/poloshirts';
+  static const String girlsshirtspants = '/products/girls/shirtpants';
+  static const String girlshoodies = '/products/girls/hoodies';
+  static const String girlshortscargo = '/products/girls/shortscargo';
+  static const String girlsjackets = '/products/girls/jackets';
+  static const String girlssweaters = '/products/girls/sweaters';
 
-
-
-  static String services = '/services';
-  static String globalsources = '/services/globalsources';
-  static String development = '/services/development';
-  static String quality = '/services/quality';
-  static String clients = '/clients';
-  static String contact = '/contact';
-
+  static const String services = '/services';
+  static const String globalsources = '/services/globalsources';
+  static const String development = '/services/development';
+  static const String quality = '/services/quality';
+  static const String clients = '/clients';
+  static const String contact = '/contact';
 
   static List<GetPage<dynamic>> pages = [
     GetPage(name: home, page: () => HomePageRoot()),
